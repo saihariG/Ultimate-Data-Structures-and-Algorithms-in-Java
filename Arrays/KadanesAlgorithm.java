d2fa8b6afdec8c17e3723dc16df358e2170b1a76
Sai Hari <krishnansaihari@gmail.com>
2021-10-28 16:44:35 +0530

    added array algorithms & readme

---
+++ Arrays/KadanesAlgorithm.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+// Largest Sum in a given array
+public class KadenesAlgorithm {
+
+     public static void main(String[] args) {
+
+     }
+
+}
---
+++ Arrays/SmallestDifference.java
@@ -0,0 +1,2 @@
+package Arrays;public class SmallestDifference {
+}
